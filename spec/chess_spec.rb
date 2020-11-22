cdba8f1823f94150136f9d3b696899f48320f552
SamGarner <samuel.w.garner@gmail.com>
2020-11-22 11:12:58 -0600

    six working tests for horizontal moves

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -25,7 +25,10 @@ describe Game do
         pawn_g1 = Pawn.new('black', [7, 6])
         @board.board_array[7][6] = pawn_g1
         @game.move_piece(@rook_h1, [7, 5])
-        expect(@game.move_piece(@rook_h1, [7, 5])).to be nil
+        expect(@board.board_array[7][7]).to eq(@rook_h1)
+        # expect(@game.move_piece(@rook_h1, [7, 5])).to be nil
+        # expect(@game.move_piece(@rook_h1, [7, 5])).to_not change(@board, :board_array)
+        # not to change
       end
 
       it 'will move if there are no pieces in the way' do
@@ -38,10 +41,25 @@ describe Game do
         expect(@board.board_array[7][7]).to eq('__')
       end
 
-      xit 'will take the opponents piece if one is on the target square' do
+      it 'will take the opponents piece if one is on the target square' do
+        pawn_h7 = Pawn.new('white', [1, 7])
+        @board.board_array[1][7] = pawn_h7
+        @game.move_piece(@rook_h1, [1, 7])
+        expect(pawn_h7.current_location).to be nil
       end
 
-      xit 'will not happen if the piece cannot move horizontally' do
+      it 'will not happen if ally is on the target square' do
+        pawn_g1 = Pawn.new('black', [7, 6])
+        @board.board_array[7][6] = pawn_g1
+        @game.move_piece(@rook_h1, [7, 6])
+        expect(@board.board_array[7][6]).to eq(pawn_g1)
+      end      
+
+      it 'will not happen if the piece cannot move horizontally' do
+        bishop_f1 = Bishop.new('black', [7, 5])
+        @board.board_array[7][5] = bishop_f1
+        @game.move_piece(bishop_f1, [7, 7])
+        expect(@board.board_array[7][5]).to eq(bishop_f1)
       end
 
       # MAKE A SPEC FILE FOR EACH PIECE TO TEST MOVEMENT ? CHECK METZ
